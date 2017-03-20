571a93a871d098ecd625b79b215ba0ad3a78db5f
한성희 <nuke82@nate.com>
2017-03-20 18:01:10 +0900

    mysql 접속테스트

--- gmp_server/routes/index.js
+++ gmp_server/routes/index.js
@@ -3,7 +3,27 @@
  */
 
 exports.index = function(req, res){
-  //res.render('index', { title: 'Express' });
+	//res.render('index', { title: 'Express' });
+	
+	var mysql      = require('mysql');
+	var connection = mysql.createConnection({
+		host     : 'tyrion.kr',
+		user     : 'root',
+		password : '1q2w3e',
+		port     : "8889",
+		database : "gmp"
+	});
+
+	connection.connect();
+
+	connection.query('SELECT * from test_table', function(err, rows, fields) {
+	  if (!err)
+	    console.log('The solution is: ', rows);
+	  else
+	    console.log('Error while performing Query.', err);
+	});
+
+	connection.end();
 	
 	res.render('login', {title : "login 화면"});
 };
